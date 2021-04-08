774d362ff428a172358b2612e1373fc02f21c149
Vitalii Tregubov <78914655+vitaliitregubov@users.noreply.github.com>
2021-04-08 15:14:36 +0300

    Add files via upload

---
+++ precache-manifest.44b0875eb80dd694f3bb9f813feb76c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dc7e9d93e6a0971b62",
+    "url": "/css/Account.29bab099.css"
+  },
+  {
+    "revision": "9a96f35741ae9d30a2ef",
+    "url": "/css/Home.d6244513.css"
+  },
+  {
+    "revision": "cc5b2d2bb42bfa014196",
+    "url": "/css/PageNotFound.9c81a615.css"
+  },
+  {
+    "revision": "e5161d88800440f31db8",
+    "url": "/css/app.221266c0.css"
+  },
+  {
+    "revision": "ae17b2400b01bc31899f",
+    "url": "/css/chunk-2de16e3d.ce899533.css"
+  },
+  {
+    "revision": "488fe83d1b5718e0b685a4aa93bb9104",
+    "url": "/img/404.488fe83d.png"
+  },
+  {
+    "revision": "e45083ee59fb719af0c42cf23765fa14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05dc7e9d93e6a0971b62",
+    "url": "/js/Account.6fbc0379.js"
+  },
+  {
+    "revision": "9a96f35741ae9d30a2ef",
+    "url": "/js/Home.c180f207.js"
+  },
+  {
+    "revision": "cc5b2d2bb42bfa014196",
+    "url": "/js/PageNotFound.c2276709.js"
+  },
+  {
+    "revision": "e5161d88800440f31db8",
+    "url": "/js/app.dc13d050.js"
+  },
+  {
+    "revision": "ae17b2400b01bc31899f",
+    "url": "/js/chunk-2de16e3d.6e9b4dd4.js"
+  },
+  {
+    "revision": "658681b7170d48039988",
+    "url": "/js/chunk-vendors.cdbea971.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
